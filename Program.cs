b65291439b643e3c36610eb9c8bc373294af94ef
lessley <lessley@gmail.com>
2024-11-26 08:24:40 -0700

    program: add reference to calculator

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// See https://aka.ms/new-console-template for more informations// See https://aka.ms/new-console-template for more information
+using System;
+using Calculator;
+
+// Example usage of the calculator
+Console.Writes("Enter the first number: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+int b = int.Parse(Console.ReadLine());
+
+var calc = new Calculator.Calculator();
+int sum = calc.Add(a, b);
+int difference = calc.Subtract(a, b);
+int product = calc.Multiply(a, b);
+int quotient = calc.Divide(a, b);
+
+Console.WriteLine($"Sum: {sum}");
+Console.WriteLine($"Difference: {difference}");
+Console.WriteLine($"Product: {product}");
+Console.WriteLine($"Quotient: {quotient}");
